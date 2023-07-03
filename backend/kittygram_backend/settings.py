3c726e444097ec3720bb273cf5c98dfbdda5d2a1
Тимофей Шмаков <Timofei.schmackov@yandex.ru>
2023-07-04 00:52:08 +0200

    v5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,9 +95,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = BASE_DIR / 'backend_static'
+STATIC_ROOT = '/app/backend_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
